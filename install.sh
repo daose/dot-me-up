42d39584d9bd88bd04317ea9cdd79d1f10a56143
daose <15278132+daose@users.noreply.github.com>
2022-03-20 23:33:08 -0400

    update

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
-#!/bin/sh
-mkdir -p ~/.config/nvim && ln -s .config/nvim/init.vim ~/.config/nvim/init.vim && ./install_nvim_plugins.sh
+#!/bin/bash
+mkdir -p ~/.config/nvim && ln -s $(pwd)/.config/nvim/init.vim ~/.config/nvim/init.vim && ./install_nvim_plugins.sh
+mkdir -p ~/.config/alacritty && ln -s $(pwd)/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -s $(pwd)/.tmux.conf ~/.tmux.conf
