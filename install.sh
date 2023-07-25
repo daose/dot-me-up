64bc17b633770a78adba4f337144c1f1f6e5e32c
daose <15278132+daose@users.noreply.github.com>
2023-07-24 22:07:14 -0400

    update

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# mkdir -p ~/.config/nvim && ln -s $(pwd)/.config/nvim/init.vim ~/.config/nvim/init.vim && ./install_nvim_plugins.sh
 # mkdir -p ~/.config/alacritty && ln -s $(pwd)/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
-# ln -s $(pwd)/.tmux.conf ~/.tmux.conf
-ln -s $(pwd)/.vimrc ~/.vimrc
+ln -s $(dirname "$0")/.tmux.conf ~/.tmux.conf
+ln -s $(dirname "$0")/.vimrc ~/.vimrc
 ./setup_vim.sh
